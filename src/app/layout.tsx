a2b4d6d13c547c0681037529eba2591ed7467081
Mihai <mihai.cira.dev@gmail.com>
2024-10-07 06:38:37 +0300

    analytics fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,38 @@ import "./globals.css";
 import Header from "../components/Header/header";
 import Background from "@/components/background/Background";
 import Footer from "@/components/Footer/Footer";
+import { Analytics } from "@vercel/analytics/react"
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+
+let analytics;
+
+if (typeof window !== 'undefined') {
+  // Only initialize analytics in the browser
+  analytics = getAnalytics(app);
+}
+
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -47,6 +79,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${poppins.variable} ${montserrat.variable}`}
       >
+        <Analytics />
         <Header />
         <Background />
         {children}
